ca767626ad7048074b6c4b0ae7ff56a0378919a9
jordanhf <jferrera@bowdoin.edu>
2019-10-21 21:04:48 -0400

    particle slight change

--- Neighborhood.java
+++ Neighborhood.java
@@ -1,8 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Neighborhood {
 
     Swarm swarm;
 
-    public Neighborhood(Swarm swarm) {
+    private List<Particle> neighbors;
+
+    public Neighborhood(Swarm swarm)
+    {
+        
         this.swarm = swarm;
     }
 
