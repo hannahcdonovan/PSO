342ff116a59722b7221677efd8abeb6c26fc02d3
Hannah Donovan <34870726+hannahcdonovan@users.noreply.github.com>
2019-10-21 20:23:47 -0400

    more skeleton work

---
+++ PSO.java
@@ -0,0 +1,17 @@
+public class PSO {
+
+    int dimension;
+
+    Function function;
+
+    Neighborhood neighborhood;
+
+    int numParticles;
+
+    public PSO(int dimension, Function function, Neighborhood neighborhood, int numParticles) {
+        this.dimension = dimension;
+        this.function = function;
+        this.neighborhood = neighborhood;
+        this.numParticles = numParticles;
+    }
+}
