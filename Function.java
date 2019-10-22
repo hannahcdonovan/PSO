0b6f1784a56bc94bb2460db221950e7ac4fb0b60
jordanhf <jferrera@bowdoin.edu>
2019-10-21 21:51:37 -0400

    Function evals for Rosenbrock and Rastrigin

--- Function.java
+++ Function.java
@@ -1,5 +1,4 @@
-
-
+import java.lang.Math.*;
 
 
 
@@ -31,5 +30,27 @@ public class Function {
 
 		return this.maxBound;
 	}
-	    
+
+    public double evalRosenbrock(Particle particle) {
+        double[] position = particle.getPos();
+        double result = 0.0;
+        for(int i = 0; i < position.length - 1; i++ ) {
+            double partOfSum = 100*Math.pow((position[i+1] - Math.pow(position[i],2)),2) + Math.pow(position[i] - 1.0, 2);
+            result += partOfSum;
+        }
+        return result
+    }
+
+    public double evalRastrigin(Particle particle) {
+        double[] position = particle.getPos();
+        int dim = particle.getDim();
+        double result = 10.0 * dim;
+        for(int i = 0; i < position.length; i++ ) {
+            double partOfSum = Math.pow(position[i], 2) - 10*Math.cos(2*Math.PI*position[i]);
+            result += partOfSum;
+        }
+        return result
+    }
+
+
 }
