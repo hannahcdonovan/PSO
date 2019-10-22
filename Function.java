33897b32e44d38806ef376cdaae38d8a76200301
Hannah Donovan <34870726+hannahcdonovan@users.noreply.github.com>
2019-10-21 21:53:15 -0400

    Ackley Function

--- Function.java
+++ Function.java
@@ -1,17 +1,10 @@
-
-
-
-
+import java.lang.Math;
 
 public class Function {
-
-
 
 	private double minBound;
 
 	private double maxBound;
-
-
 
 	public Function(double minBound, double maxBound) {
 
@@ -30,6 +23,46 @@ public class Function {
 	public double getMaxBoud() {
 
 		return this.maxBound;
-	}
+    }
+    
+    public double evalAckley(Particle particle) {
+        double[] params = particle.getPosition();
+
+        int a = 20;
+        double b = 0.2;
+        double c = 2 * Math.PI;
+        int d = particle.getDimension();
+        double inverse = 1/d;
+
+        double result = -a * Math.exp(-b * Math.sqrt(inverse * summingSquaresAckelyHelper(particle))) 
+                        - Math.exp(inverse * summingCosAckleyHelper(particle, c)) + a + Math.exp(1);
+        
+        return result;
+
+    }
+
+    public double summingSquaresAckelyHelper(Particle particle) {
+        double[] params = particle.getPosition();
+
+        double answer = 0;
+
+        for (int i = 0; i < particle.getDimension(); i++) {
+            answer += Math.pow(params[i], 2);
+        }
+        
+        return answer;
+    }
+
+    public double summingCosAckleyHelper(Partcile particle, double c) {
+        double[] params = particles.getPosition();
+        
+        double answer = 0;
+
+        for (int i = 0; i < particles.getDimension(); i++) {
+            answer += Math.cos(c*params[i]);
+        }
+
+        return answer;
+    }
 	    
 }
--- Neighborhood.java
+++ Neighborhood.java
@@ -1,7 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class Neighborhood {
 
     Swarm swarm;
