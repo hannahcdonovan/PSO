0c80990a2bc5a05264e67dc8ee4a53276a16f71e
Jack O'Connor <jackoco@me.com>
2019-10-21 21:25:25 -0400

    Particle and Function updates

--- Function.java
+++ Function.java
@@ -1,3 +1,35 @@
+
+
+
+
+
 public class Function {
-    
+
+
+
+	private double minBound;
+
+	private double maxBound;
+
+
+
+	public Function(double minBound, double maxBound) {
+
+		this.minBound = minBound;
+		this.maxBound = maxBound;
+
+
+	}
+
+
+	public double getMinBound() {
+
+		return this.minBound;
+	}
+
+	public double getMaxBoud() {
+
+		return this.maxBound;
+	}
+	    
 }
--- Particle.java
+++ Particle.java
@@ -1,11 +1,38 @@
 public class Particle {
-    int gBest;
 
-    int pBest;
 
-    int nBest;
+	private int dimensions;
 
-    public Particle() {
+    private double[] position;
+
+   	private double[] velocity;
+
+    private double[] personalBest;
+
+    private double[] neighborhoodBest;
+
+    private Neighborhood neighborhood;
+
+    private Function func;
+
+    public Particle(int dimensions, Function func) {
         
+        this.dimensions = dimensions;
+        this.position = new double[dimensions];
+        this.velocity = new double[dimensions];
+        this.personalBest = new double[dimensions];
+        this.neighborhoodBest = new double[dimensions];
+        this.func = func;
+
+    }
+
+
+    public void generateRandomPosition() {
+
+    	double minBound = this.func.getMinBound();
+    	double maxBound = this.func.getMaxBound();
+
+    	
+
     }
 }
